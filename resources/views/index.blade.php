1e32a602b9b8bd7214e40d45cba827ead7689b85
VaguelySerious <mittgfu@gmail.com>
2020-01-26 10:44:49 +0100

    frontend: fix embarassing typo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
 @endcomponent
 <div class="container">
     <h1 class="title">The great catalog</h1>
-    <h2 class="subtitle">Nothing greated has ever existed</h2>
+    <h2 class="subtitle">Nothing greater has ever existed</h2>
     <p>Put all you products into a nice catalog and never have to worry about them again. Except if you want to change them. Or want to keep track of stock. Or need integration with third party services. Alright it's not that useful.</p>
 </div>
 @endsection
