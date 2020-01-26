3a757f6310f8e1664bb8c020bd36a6b1e0c45832
VaguelySerious <mittgfu@gmail.com>
2020-01-26 08:41:54 +0100

    frontend: add Bulma css framework via resource in <head>, remove laravel default font

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,8 @@
         <title>Catalog - @yield('title')</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+        <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
         <link rel="stylesheet" href="/styles.css">
     </head>
     <body>
